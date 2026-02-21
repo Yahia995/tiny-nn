06398023aa7f904adb3a0ec8283bd88f88f1a20d
Yahia995 <yahia995ach@gmail.com>
2026-02-21 22:56:01 +0100

    chore: initialize  tiny-nn project with CMake and main entry

---
+++ build/CMakeFiles/tiny_nn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tiny_nn.dir/link.d"
+  "CMakeFiles/tiny_nn.dir/src/main.cpp.o"
+  "CMakeFiles/tiny_nn.dir/src/main.cpp.o.d"
+  "tiny_nn"
+  "tiny_nn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tiny_nn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Tiny NN starting..." << std::endl;
+    return 0;
+}
